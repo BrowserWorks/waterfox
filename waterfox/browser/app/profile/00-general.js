99f7e863ea68c6cf3baed87a03bcf598e8e74b4c
Alex Kontos <MrAlex94@users.noreply.github.com>
2024-03-28 15:57:56 +0000

    feat: add option to disable close button on tabs
    (cherry picked from commit ff68d8fd9ff678e6cf7efb6d662728b223322ba4)

--- waterfox/browser/app/profile/00-general.js
+++ waterfox/browser/app/profile/00-general.js
@@ -32,6 +32,7 @@ pref("app.update.url.override", "", sticky); // Override URL for application upd
 // Defines browser behavior on startup and general UI settings.
 pref("browser.startup.page", 3); // 0=blank, 1=home, 2=last visited page, 3=resume previous session.
 pref("browser.privateTab.showNewTabButton", false); // Show new private tab button next to existing new tab buttons
+pref("browser.tabs.closeButtons", false); // Hide close button on tabs
 pref("browser.tabs.pinnedIconOnly", true); // Pinned tabs show only an icon, no text.
 pref("browser.tabs.warnOnClose", true); // Warn user when attempting to close multiple tabs.
 // Stores the state of toolbar customizations (e.g., button placements).
