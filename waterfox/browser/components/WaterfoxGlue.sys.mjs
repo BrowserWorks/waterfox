5f405c9176c5fe475a1f21079766198f2dd5814c
Alex Kontos <MrAlex94@users.noreply.github.com>
2026-06-11 19:58:42 +0100

    feat(private-tabs): add private tabs

--- waterfox/browser/components/WaterfoxGlue.sys.mjs
+++ waterfox/browser/components/WaterfoxGlue.sys.mjs
@@ -5,6 +5,7 @@
 const lazy = {};
 
 ChromeUtils.defineESModuleGetters(lazy, {
+  PrivateTab: "resource:///modules/PrivateTab.sys.mjs",
   StatusBar: "resource:///modules/StatusBar.sys.mjs",
   StyleSheetUtils: "resource:///modules/StyleSheetUtils.sys.mjs",
   TabFeatures: "resource:///modules/TabFeatures.sys.mjs",
@@ -110,6 +111,7 @@ export const WaterfoxGlue = {
       console.error("WaterfoxBlockerService startup init failed", error)
     );
 
+    lazy.PrivateTab.init();
     lazy.StatusBar.init();
     lazy.TabFeatures.init();
     lazy.TabGrouping.init();
@@ -120,6 +122,7 @@ export const WaterfoxGlue = {
   observe(subject, topic) {
     switch (topic) {
       case "browser-delayed-startup-finished":
+        lazy.PrivateTab.onWindowOpened(subject);
         lazy.StatusBar.onWindowOpened(subject);
         lazy.TabFeatures.onWindowOpened(subject);
         lazy.TabGrouping.onWindowOpened(subject);
