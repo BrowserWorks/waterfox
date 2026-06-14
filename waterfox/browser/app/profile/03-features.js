6575ca36b8915109080b2ca2b87e40ee2bd59275
Alex Kontos <MrAlex94@users.noreply.github.com>
2026-06-14 16:15:19 +0100

    feat(update): major upgrade, part 2 - show the upgrade dialog

--- waterfox/browser/app/profile/03-features.js
+++ waterfox/browser/app/profile/03-features.js
@@ -71,6 +71,8 @@ pref("browser.search.param.waterfox_attribution_ddg", "waterfox");
 pref("browser.search.param.waterfox_attribution_ecosia", "57226k1p");
 pref("browser.search.param.waterfox_attribution_qwant", "brz-waterfox");
 
+pref("browser.startup.upgradeDialog.enabled", true);
+
 pref("sidebar.revamp", true);
 pref("sidebar.revamp.defaultLauncherVisible", true);
 pref("sidebar.autoHide", false);
