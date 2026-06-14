81fc79ab038055e2c996dfd35943fe5bcc2d078b
Alex Kontos <MrAlex94@users.noreply.github.com>
2026-06-14 12:00:08 +0100

    feat(migration): add Firefox profile import migrator

--- browser/components/migration/MigrationWizardChild.sys.mjs
+++ browser/components/migration/MigrationWizardChild.sys.mjs
@@ -83,7 +83,10 @@ export class MigrationWizardChild extends JSWindowActorChild {
     switch (event.type) {
       case "MigrationWizard:RequestState": {
         this.#sendTelemetryEvent("opened");
-        await this.#requestState(event.detail?.allowOnlyFileMigrators);
+        await this.#requestState(
+          event.detail?.allowOnlyFileMigrators,
+          event.detail?.migratorKey
+        );
         break;
       }
 
@@ -174,12 +177,12 @@ export class MigrationWizardChild extends JSWindowActorChild {
     }
   }
 
-  async #requestState(allowOnlyFileMigrators) {
+  async #requestState(allowOnlyFileMigrators, migratorKey) {
     this.setComponentState({
       page: MigrationWizardConstants.PAGES.LOADING,
     });
 
-    await this.#populateMigrators(allowOnlyFileMigrators);
+    await this.#populateMigrators(allowOnlyFileMigrators, migratorKey);
 
     this.#wizardEl.dispatchEvent(
       new this.contentWindow.CustomEvent("MigrationWizard:Ready", {
@@ -317,7 +320,7 @@ export class MigrationWizardChild extends JSWindowActorChild {
    *   message.
    */
   async beginMigration(migrationDetails, extraArgs) {
-    // We redirect to manual password import for Safari and Chrome on Windows.
+    // We redirect to manual password import for browsers that require a CSV file.
     if (
       migrationDetails.resourceTypes.includes(
         MigrationWizardConstants.DISPLAYED_RESOURCE_TYPES.PASSWORDS
