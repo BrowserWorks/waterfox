6b7dbb0df98b84b1148a4cae818302e7221cf94b
Alex Kontos <MrAlex94@users.noreply.github.com>
2026-07-29 12:54:57 +0100

    feat(tabs): part 1 - add tab display preferences

--- waterfox/browser/components/WaterfoxGlue.sys.mjs
+++ waterfox/browser/components/WaterfoxGlue.sys.mjs
@@ -7,6 +7,7 @@ const lazy = {};
 ChromeUtils.defineESModuleGetters(lazy, {
   StatusBar: "resource:///modules/StatusBar.sys.mjs",
   StyleSheetUtils: "resource:///modules/StyleSheetUtils.sys.mjs",
+  TabFeatures: "resource:///modules/TabFeatures.sys.mjs",
   UICustomizations: "resource:///modules/UICustomizations.sys.mjs",
   WaterfoxBlockerExtensionDetector:
     "resource:///modules/WaterfoxBlockerExtensionDetector.sys.mjs",
@@ -109,6 +110,7 @@ export const WaterfoxGlue = {
     );
 
     lazy.StatusBar.init();
+    lazy.TabFeatures.init();
     lazy.UICustomizations.init();
     Services.obs.addObserver(this, "browser-delayed-startup-finished");
   },
